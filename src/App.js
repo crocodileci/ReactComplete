18f59b9990690a085e2937709ceab7a6577b520e
Grady Dun <gradydun@hitrust.com.tw>
2020-05-02 17:38:44 +0800

    新增一個component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
+  
+  // 如果使用以下JSX語法須引入React
+  // JSX 語法有兩個需要注意的地方
+  // 1. 如果要指定 CSS class 只能使用className來指定，因為 class 是Javascript的關鍵字
+  // 2. 只能有一個根元素，因為他是
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>I'm a React App</h1>
+      <p> This is really working!! </p>
+      <Person />
+      <Person />
+      <Person />
     </div>
   );
+  
+  // 以上的JSX語法會被編譯成以下的程式碼，用來動態的產生對應的dom元件
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m a React App'));
 }
 
 export default App;
