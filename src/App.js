2d6a7c6dbf7adbcbedb42ebf2635415c8e1a316c
Grady Dun <gradydun@hitrust.com.tw>
2020-05-03 15:49:47 +0800

    Componet 如果要使用state則需要使用 classed base的方式

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
+class App extends Component{
+
+  state = {
+    persons : [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26}
+    ]
+  }
   
   // 如果使用以下JSX語法須引入React
   // JSX 語法有兩個需要注意的地方
   // 1. 如果要指定 CSS class 只能使用className來指定，因為 class 是Javascript的關鍵字
   // 2. 只能有一個根元素，因為他是
-  return (
-    <div className="App">
-      <h1>I'm a React App</h1>
-      <p> This is really working!! </p>
-      <Person name="Max" age="28"/>
-      <Person name="Manu" age="29">My Hobbies: Racing</Person>
-      <Person name="Stephanie" age="26"/>
-    </div>
-  );
+  render(){
+    return (
+      <div className="App">
+        <h1>I'm a React App</h1>
+        <p> This is really working!! </p>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    );
+  }
   
   // 以上的JSX語法會被編譯成以下的程式碼，用來動態的產生對應的dom元件
   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m a React App'));
